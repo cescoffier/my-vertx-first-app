3229acffae651451ed5d1dfcaf63af28cdd820ee
Clement Escoffier <clement.escoffier@gmail.com>
2016-10-21 09:10:21 +0200

    add configuration parameter to the script

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn package -DskipTests; java -jar target/my-first-app-1.0-SNAPSHOT-fat.jar
+mvn package -DskipTests; java -jar target/my-first-app-1.0-SNAPSHOT-fat.jar -conf src/main/conf/my-application-conf.json
